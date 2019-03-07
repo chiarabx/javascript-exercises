07c7713138e97267dfddda8584fcef40d99d2ebe
Valerio Coltre <valerio.coltre@gmail.com>
2019-03-07 16:16:10 +0100

    converted spy tests to Karma

--- exercises/Steps/test.js
+++ exercises/Steps/test.js
@@ -2,11 +2,11 @@
 
 describe('Steps function', () => {
   beforeEach(() => {
-    jest.spyOn(console, 'log');
+    spyOn(console, 'log');
   });
 
   afterEach(() => {
-    console.log.mockRestore();
+    console.log.calls.reset();
   });
 
   it('should exists', () => {
@@ -16,22 +16,22 @@ describe('Steps function', () => {
 
   it('should prints steps when called with 1', () => {
     steps(1);
-    expect(console.log.mock.calls[0][0]).toEqual('#');
-    expect(console.log.mock.calls.length).toEqual(1);
+    expect(console.log.calls.argsFor(0)[0]).toEqual('#');
+    expect(console.log.calls.count()).toEqual(1);
   });
 
   it('should prints steps when called with 2', () => {
     steps(2);
-    expect(console.log.mock.calls[0][0]).toEqual('# ');
-    expect(console.log.mock.calls[1][0]).toEqual('##');
-    expect(console.log.mock.calls.length).toEqual(2);
+    expect(console.log.calls.argsFor(0)[0]).toEqual('# ');
+    expect(console.log.calls.argsFor(1)[0]).toEqual('##');
+    expect(console.log.calls.count()).toEqual(2);
   });
 
   it('should prints steps when called with 3', () => {
     steps(3);
-    expect(console.log.mock.calls[0][0]).toEqual('#  ');
-    expect(console.log.mock.calls[1][0]).toEqual('## ');
-    expect(console.log.mock.calls[2][0]).toEqual('###');
-    expect(console.log.mock.calls.length).toEqual(3);
+    expect(console.log.calls.argsFor(0)[0]).toEqual('#  ');
+    expect(console.log.calls.argsFor(1)[0]).toEqual('## ');
+    expect(console.log.calls.argsFor(2)[0]).toEqual('###');
+    expect(console.log.calls.count()).toEqual(3);
   });
 });
